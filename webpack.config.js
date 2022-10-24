d4c27241afd95e4ce34f10eb9199fa31bb731c0c
Andylarchin <stillascold@gmail.com>
2022-10-24 15:44:15 +0300

    please help me

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,56 @@
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-// const path = require('path');
-// const { dependencies } = require('./package.json');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const path = require('path');
+const { dependencies } = require('./package.json');
 
-// module.exports = {
-//   entry: './src/index',
-//   mode: 'development',
-//   devServer: {
-//     static: {
-//       directory: path.join(__dirname, 'public'),
-//     },
-//     port: 3000,
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)?$/,
-//         exclude: /node_modules/,
-//         use: [
-//           {
-//             loader: 'babel-loader',
-//             options: {
-//               presets: ['@babel/preset-env', '@babel/preset-react'],
-//             },
-//           },
-//         ],
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new ModuleFederationPlugin({
-//       name: 'Host',
-//       remotes: {
-//         remote: 'LoginForm@http://localhost:4001/remoteEntry.js',      },
-//       shared: {
-//         ...dependencies,
-//         react: {
-//           singleton: true,
-//           requiredVersion: dependencies['react'],
-//         },
-//         'react-dom': {
-//           singleton: true,
-//           requiredVersion: dependencies['react-dom'],
-//         },
-//       },
-//     }),
-//     new HtmlWebpackPlugin({
-//       template: './public/index.html',
-//     }),
-//   ],
-//   resolve: {
-//     extensions: ['.js', '.jsx'],
-//   },
-//   target: 'web',
-// };
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'Host',
+      remotes: {
+        remote: 'LoginForm@http://localhost:4001/remoteEntry.js',      },
+      shared: {
+        ...dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: dependencies['react'],
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: dependencies['react-dom'],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  target: 'web',
+};
